736713ebf6e73329ac9884a3770940d235409315
Joey Aghion <joey@aghion.com>
2016-01-13 23:00:39 -0500

    Fix #21: don't delete keys from the attributes hash when rendering config

--- libraries/mongodb3_helper.rb
+++ libraries/mongodb3_helper.rb
@@ -1,14 +1,11 @@
 
 module Mongodb3Helper
   def mongodb_config(config)
-    config_hash = config.to_hash
-    config_hash.compact
-    JSON.parse(config_hash.dup.to_json).to_yaml
+    config_hash = compact_hash(config.to_hash)
+    JSON.parse(config_hash.to_json).to_yaml
   end
-end
 
-class Hash
-  def compact
-    delete_if {|k,v| v.is_a?(Hash) ? v.compact.empty? : v.nil? }
+  def compact_hash(hash)
+    hash.reject{|k,v| v.is_a?(Hash) ? compact_hash(v).empty? : v.nil? }
   end
 end
